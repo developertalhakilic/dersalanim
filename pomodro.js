b8fc0da3d909e5f372fce4a7c8c5277d797706f4
sanalsozluk <wolfappsoffical@gmail.com>
2024-02-25 20:20:33 +0300

    s

--- pomodro.js
+++ pomodro.js
@@ -3,6 +3,12 @@ const pomodroContainer = document.getElementById("pomodroContainer")
 const icerik1 = document.getElementById("icerik1")
 const icerik2 = document.getElementById("icerik2")
 const logo = document.getElementById("logo")
+const dakikacon = document.getElementById("dakika")
+const saniyecon = document.getElementById("saniye")
+const baslatButon = document.getElementById("butonBaslat")
+const duraklatButon = document.getElementById("butonDuraklat")
+const bitirButon = document.getElementById("butonBitir")
+const restartButon = document.getElementById("butonGeriBaslat")
 function tamEkran() {
     var element = document.documentElement; // Sayfanın kök elementini alı
     if (element.requestFullscreen) {
@@ -43,3 +49,55 @@ tamEkranButton.addEventListener('click', () => {
         });
     }
 });
+
+function zamanlayiciBaslat(){
+    if(saniyecon.innerText > 0){
+        let saniyeconN = Number(saniyecon.innerText)
+        saniyeconN-=1
+        saniyecon.innerText = saniyeconN
+    }
+    else if(dakikacon.innerText > 0){
+        let dakikaconN= Number(dakikacon.innerText)
+        dakikaconN-=1
+        dakikacon.innerText = dakikaconN
+        saniyecon.innerText = "59"
+    }
+    else{
+        saniyecon.innerText = "59"
+        dakikacon.innerText = "04"
+    }
+}
+
+baslatButon.addEventListener('click', function() {
+    zamanlayici = setInterval(zamanlayiciBaslat, 1000);
+});
+duraklatButon.addEventListener('click', function() {
+    clearInterval(zamanlayici)
+});
+restartButon.addEventListener('click', function() {
+    zamanlayici = setInterval(zamanlayiciBaslat, 1000);
+});
+bitirButon.addEventListener('click', function() {
+    clearInterval(zamanlayici)
+    dakikacon.innerText = "25"
+    saniyecon.innerText = "00"
+});
+function hideStartButton(){
+    baslatButon.style.display = "none"
+    duraklatButon.style.display = "inline-block"
+    bitirButon.style.display = "inline-block"
+}
+function showTheBaslatButon(){
+    restartButon.style.display = "inline-block"
+    duraklatButon.style.display = "none"
+}
+function showTheDuraklatButon(){
+    restartButon.style.display = "none"
+    duraklatButon.style.display = "inline-block"
+}
+function stopButton(){
+    baslatButon.style.display = "inline-block"
+    duraklatButon.style.display = "none"
+    bitirButon.style.display = "none"
+    restartButon.style.display = "none"
+}
