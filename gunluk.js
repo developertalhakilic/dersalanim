88aacf189fd4747c2bc423046e56c17a0c4e4c18
sanalsozluk <wolfappsoffical@gmail.com>
2024-04-30 22:36:46 +0300

    Günlük öğelerini silme ve güncelleme özelliği eklendi. ( BÜYÜK GÜNCELLEME )

--- gunluk.js
+++ gunluk.js
@@ -2,6 +2,17 @@ const tarihYazi = document.getElementById("tarih")
 const baslikYazi = document.getElementById("baslik")
 const icerikYazi = document.getElementById("icerik")
 const sayfalarDiv = document.getElementById("sayfalarDiv")
+
+const iptalButton = document.getElementById("iptalButton")
+const silmeButton = document.getElementById("silmeButton")
+const silmeIcerikOnayYazi = document.getElementById("icerikOnay")
+const silmeOnayButton = document.getElementById("silmeOnayButton")
+const sayfaDuzenleButton = document.getElementById("sayfaDuzenleButton")
+const duzenlemeBaslik = document.getElementById("duzenlemeBaslik")
+const duzenlemeBaslikInput = document.getElementById("duzenlemeBaslikInput")
+const duzenlemeTarihInput = document.getElementById("duzenlemeTarihInput")
+const duzenlemeIcerikInput = document.getElementById("duzenlemeIcerikInput")
+const sayfayiDuzenleButton = document.getElementById("sayfayiDuzenleButton")
 let gunlukHTML = localStorage.getItem("GunlukHTML")
 if(gunlukHTML != null){
   sayfalarDiv.innerHTML = `${gunlukHTML}`
@@ -10,9 +21,12 @@ if(gunlukHTML != null){
 if(localStorage.getItem("yaziTarihleri") == null){
   localStorage.setItem("yaziTarihleri","")
 }
+if(localStorage.getItem("silinenIdler") == null){
+  localStorage.setItem("silinenIdler","")
+}
 localStorage.setItem("icerikYaziValueKey0",0)
 localStorage.setItem("baslikYaziValueKey0",0)
-localStorage.setItem("tarihYaziValueKeyKey0",0)
+localStorage.setItem("tarihYaziValueKey0",0)
 function yeniSayfaEkle(){
     let olusturulacakDivler = ""
     let tumLocalKeyler = Object.keys(localStorage)
@@ -48,6 +62,9 @@ function yeniSayfaEkle(){
     }
     sayfalarDiv.innerHTML = ""
     for(let i = 1; i<=enBuyuk+1; i++){ 
+        if(localStorage.getItem("silinenIdler").includes(i)){
+          continue
+        }
         if(tumLocalKeyler.includes(`tarihYaziValueKey${i}`) == false){
             localStorage.setItem("tarihYaziValueKey" + i, tarihYaziValue)
         }
@@ -58,11 +75,11 @@ function yeniSayfaEkle(){
             localStorage.setItem("icerikYaziValueKey" + i, icerikYaziValue)
         }
     olusturulacakDivler = `
-    <div class="accordion accordion-flush" id="accordionFlushExample">
+    <div class="accordion accordion-flush" id="gunlukElement${i}">
     <div class="accordion-item">
       <h2 class="accordion-header">
         <button class="accordion-button collapsed acilanMenu" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
-        <h5 class="menuBaslik">${localStorage.getItem("tarihYaziValueKey" + i) + " - " + bugununGunu(localStorage.getItem("tarihYaziValueKey" + i))}</h5>
+        <h5 class="menuBaslik"> <span id="menuTarih${i}">${localStorage.getItem("tarihYaziValueKey" + i)} </span> - <span id="menuTarihGun${i}">${bugununGunu(localStorage.getItem("tarihYaziValueKey" + i))}</span></h5>
         </button>
         <!--<button>
         <i class="bi bi-trash3"></i>
@@ -70,12 +87,12 @@ function yeniSayfaEkle(){
       </h2>
       <div id="collapse${i}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
         <div class="accordion-body icerikBody">
-          <h3 class="icerikBaslik">${localStorage.getItem("baslikYaziValueKey" + i)}</h3> <br> <p class="icerikP">${localStorage.getItem("icerikYaziValueKey" + i)}</p>
+          <h3 class="icerikBaslik"  id="menuBaslik${i}">${localStorage.getItem("baslikYaziValueKey" + i)}</h3> <br> <p class="icerikP" id="menuIcerik${i}">${localStorage.getItem("icerikYaziValueKey" + i)}</p>
           <div class="buttonContainer">
-          <button class="editButton">
+          <button class="editButton" onclick="elementSil(${i})">
           <i class="bi bi-trash2"></i>
           </button>
-          <button class="editButton">
+          <button class="editButton" onclick="elementDuzenle(${i},${localStorage.getItem("tarihYaziValueKey" + i)})">
           <i class="bi bi-pencil-square"></i>
           </button>
           </div>
@@ -86,6 +103,44 @@ function yeniSayfaEkle(){
 
 }
     localStorage.setItem("GunlukHTML",sayfalarDiv.innerHTML)
+}
+
+
+function elementDuzenle(elementId){
+  sayfaDuzenleButton.click()
+  let gun = bugununGunu(localStorage.getItem("tarihYaziValueKey" + elementId))
+  duzenlemeBaslik.innerText = '"' + gun + " - " + localStorage.getItem("tarihYaziValueKey" + elementId) + '"' + " Gününü Düzenle"
+  duzenlemeBaslikInput.value = localStorage.getItem("baslikYaziValueKey" + elementId)
+  duzenlemeTarihInput.value = localStorage.getItem("tarihYaziValueKey" + elementId)
+  duzenlemeIcerikInput.value = localStorage.getItem("icerikYaziValueKey" + elementId)
+  sayfayiDuzenleButton.addEventListener("click",function(){
+  localStorage.setItem(`tarihYaziValueKey${elementId}`,duzenlemeTarihInput.value)
+  localStorage.setItem(`baslikYaziValueKey${elementId}`,duzenlemeBaslikInput.value)
+  localStorage.setItem(`icerikYaziValueKey${elementId}`,duzenlemeIcerikInput.value)
+  document.getElementById(`menuBaslik${elementId}`).innerText = localStorage.getItem(`baslikYaziValueKey${elementId}`)
+  document.getElementById(`menuIcerik${elementId}`).innerText = localStorage.getItem(`icerikYaziValueKey${elementId}`)
+  document.getElementById(`menuTarih${elementId}`).innerText = localStorage.getItem(`tarihYaziValueKey${elementId}`)
+  document.getElementById(`menuTarihGun${elementId}`).innerText = bugununGunu(localStorage.getItem(`tarihYaziValueKey${elementId}`,duzenlemeTarihInput.value))
+  localStorage.setItem("GunlukHTML",sayfalarDiv.innerHTML)
+  })
+}
+function elementSil(elementId){
+  let gun = bugununGunu(localStorage.getItem("tarihYaziValueKey" + elementId))
+  silmeOnayButton.click()
+  silmeIcerikOnayYazi.innerHTML =  `
+  <label for="message-text" class="col-form-label text-white">
+  <b>${'"' + gun + " - " + localStorage.getItem("tarihYaziValueKey" + elementId) + '"'}</b> Günü silinecek emin misiniz?
+  </label>
+  `
+  silmeButton.addEventListener("click",function(){
+    document.getElementById(`gunlukElement${elementId}`).remove()
+    localStorage.setItem("GunlukHTML",sayfalarDiv.innerHTML)
+    let silinenIdListe = localStorage.getItem("silinenIdler")
+    silinenIdListe += elementId
+    localStorage.setItem("silinenIdler", silinenIdListe)
+    iptalButton.click()
+  })
+
 }
 function bugununTarihi(){
     var today = new Date();
@@ -100,6 +155,7 @@ function bugununTarihi(){
     }
     var bugununTarihi = year + '-' + month + '-' + day;
     document.getElementById('tarih').value = bugununTarihi;
+    duzenlemeTarihInput.value = bugununTarihi
 }
 function bugununGunu(tarih){
   let gun = ""
@@ -110,3 +166,4 @@ function bugununGunu(tarih){
   let gunler = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"]
   return gunler[tarihBugun.getDay()]
 }
+
