3d9815ba124ef291dfeb6247e33495137bdcf4c7
sanalsozluk <wolfappsoffical@gmail.com>
2024-05-27 19:48:05 +0300

    Hata düzeltimi

--- stats.js
+++ stats.js
@@ -1,5 +1,5 @@
-const ctx = document.getElementById('pomodoro');
-var data = {
+const ctx = document.getElementById('timer');
+var dataZamanTutucuUp1000 = {
     labels: ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar'],
     datasets: [{
         label: 'Dakika',
@@ -25,32 +25,81 @@ var data = {
           borderWidth: 1
     }]
 };
-
-// Grafik oluşturma
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: data,
-    options: {
-        title: {
-            display: true,
-            text: 'Zaman Tutucu İstatistikleri',
-            fontColor: 'red', // Başlık rengi
-            fontSize: 20 // Başlık font boyutu
-        },
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    color: '#202020' // Sütun çizgilerinin rengi (kırmızı renk örneği)
-                }
-            }],
-            yAxes: [{
-                gridLines: {
-                    color: '#202020' // Sütun çizgilerinin rengi (yeşil renk örneği)
-                }
-            }],
-            y: {
-              beginAtZero: true
-            }
-          }      
-    }
+var dataZamanTutucuDown1000 = {
+    labels: ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar'],
+    datasets: [{
+        label: 'Dakika',
+        data: [Number(localStorage.getItem("zamanTutucuPazartesiVerisi")), Number(localStorage.getItem("zamanTutucuSaliVerisi")), Number(localStorage.getItem("zamanTutucuCarsambaVerisi")), Number(localStorage.getItem("zamanTutucuPersembeVerisi")), Number(localStorage.getItem("zamanTutucuCumaVerisi")), Number(localStorage.getItem("zamanTutucuCumartesiVerisi")), Number(localStorage.getItem("zamanTutucuPazarVerisi"))],
+        backgroundColor: 'rgba(54, 162, 235, 0.2)', // Dolgu rengi
+        borderColor: 'rgba(54, 162, 235, 1)', // Kenarlık rengi
+        borderWidth: 1
+    }]
+};
+let kontrolSayi;
+window.addEventListener('resize', function(){
+    timerChartOlustur()
 });
+timerChartOlustur()
+function timerChartOlustur(){
+    if(window.innerWidth > 1000){
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: dataZamanTutucuUp1000,
+            options: {
+                title: {
+                    display: true,
+                    text: 'Zaman Tutucu İstatistikleri',
+                    fontColor: 'red', // Başlık rengi
+                    fontSize: 20 // Başlık font boyutu
+                },
+                scales: {
+                    xAxes: [{
+                        gridLines: {
+                            color: '#202020' // Sütun çizgilerinin rengi (kırmızı renk örneği)
+                        }
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            color: '#202020' // Sütun çizgilerinin rengi (yeşil renk örneği)
+                        }
+                    }],
+                    y: {
+                      beginAtZero: true
+                    }
+                  }      
+            }
+        });
+        
+    }
+    else{
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: dataZamanTutucuDown1000,
+            options: {
+                title: {
+                    display: true,
+                    text: 'Zaman Tutucu İstatistikleri',
+                    fontColor: 'red', // Başlık rengi
+                    fontSize: 20 // Başlık font boyutu
+                },
+                scales: {
+                    xAxes: [{
+                        gridLines: {
+                            color: '#202020' // Sütun çizgilerinin rengi (kırmızı renk örneği)
+                        }
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            color: '#202020' // Sütun çizgilerinin rengi (yeşil renk örneği)
+                        }
+                    }],
+                    y: {
+                      beginAtZero: true
+                    }
+                  }      
+            }
+        });
+        
+    }
+}
+// Grafik oluşturma
