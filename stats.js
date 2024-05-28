f8631711d46dcaa2fdf315eb11f01be7195704f4
sanalsozluk <wolfappsoffical@gmail.com>
2024-05-28 21:09:47 +0300

    Hata düzeltimi

--- stats.js
+++ stats.js
@@ -1,3 +1,25 @@
+const pomodoroDakikaYazi = document.getElementById("pomodoroDakikaYazi")
+const zamanlayiciDakikaYazi = document.getElementById("zamanlayiciDakikaYazi")
+const soruSayiYazi = document.getElementById("soruSayiYazi")
+const yapilacakİsYazi = document.getElementById("yapilacakİsYazi")
+const sayfaSayiYazi = document.getElementById("sayfaSayiYazi")
+
+if(localStorage.getItem("pomodoroDersDakikasi") != null){
+    pomodoroDakikaYazi.innerText = localStorage.getItem("pomodoroDersDakikasi")
+}
+if(localStorage.getItem("zamanlayiciDersDakikasi") != null){
+    zamanlayiciDakikaYazi.innerText = localStorage.getItem("zamanlayiciDersDakikasi")
+}
+if(localStorage.getItem("kaydedilenSoru") != null){
+    soruSayiYazi.innerText = localStorage.getItem("kaydedilenSoru")
+}
+// if(localStorage.getItem("pomodoroDersDakikasi") != null){
+//     yapilacakİsYazi.innerText = localStorage.getItem("pomodoroDersDakikasi")
+// }
+if(localStorage.getItem("pomodoroDersDakikasi") != null){
+    sayfaSayiYazi.innerText = localStorage.getItem("pomodoroDersDakikasi")
+}
+
 const timerDiv = document.getElementById('timer');
 var dataZamanTutucuUp1000 = {
     labels: ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar'],
